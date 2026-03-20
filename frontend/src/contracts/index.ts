12eed2ea58754b83c10d80aa4969469d2f1521ca
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2026-03-20 03:18:56 +0100

    chore: Update all contract addresses and reconfigure token definitions in the contract index.

--- frontend/src/contracts/index.ts
+++ frontend/src/contracts/index.ts
@@ -5,13 +5,13 @@ import pythAbi from './abis/Pyth.json';
 
 export const CONTRACTS = {
   TIDE_HOOK: {
-    address: '0xd6d82258e3c0d0c7b24d7284791dd5979206d0c8' as const,
+    address: '0x3e80303c7ad85b43ef77ad950defcab2ed4850c8' as const,
     abi: tideHookAbi.abi,
   },
-  POOL_MANAGER: '0xf2f57549bb79b00aa11e44716d9a3ec34452dc27' as const,
-  SWAP_ROUTER: '0xa97a9202b131f94050f38dee5dd8b3393b0d2367' as const,
-  TOKEN_0: '0x2f9debe9d9fa5d076505e8f60c69af0cc6646d9a' as const, // ETH
-  TOKEN_1: '0x717c07cdeaa2b733e3a5675da4cf60192e7d16b4' as const, // USDC
+  POOL_MANAGER: '0x43821919c6a1abd93e91e10ddbfa067906708ddc' as const,
+  SWAP_ROUTER: '0x509752a7f94dc1974a7e0ad2c3353bf7da6045c3' as const,
+  TOKEN_0: '0x08f64c5a9d611016918a32d4598ac0e2b86569ff' as const, // USDC
+  TOKEN_1: '0x7a5e743ec10ed2f2ffd410a408757e262a4428c7' as const, // ETH
   REACTIVE_NETWORK: '0x3Be7fbBDbC73Fc4731D60EF09c4BA1A94DC58E41' as const,
   PYTH_ORACLE: {
     address: '0x2880aB155794e7179c9eE2e38200202908C17B43' as const,
@@ -26,16 +26,16 @@ export const CONTRACTS = {
 export const TOKEN_LIST = [
   {
     address: CONTRACTS.TOKEN_0,
-    symbol: 'ETH',
-    name: 'Mock ETH',
-    decimals: 18,
-    logo: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
-  },
-  {
-    address: CONTRACTS.TOKEN_1,
     symbol: 'USDC',
     name: 'Mock USDC',
     decimals: 18,
     logo: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+  },
+  {
+    address: CONTRACTS.TOKEN_1,
+    symbol: 'ETH',
+    name: 'Mock ETH',
+    decimals: 18,
+    logo: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
   },
 ];
